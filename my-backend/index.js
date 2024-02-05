4139ab191d6159b800fd35992346538e83d59fc8
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2024-02-05 21:59:29 +0500

    Updated

--- my-backend/index.js
+++ my-backend/index.js
@@ -1,1 +1,21 @@
-exp
+const express = require('express');
+const dotenv = require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+
+app.get("/api/msg", (req,res)=>{
+
+    res.json({
+        message: 'Hello there'
+    });
+})
+
+app.listen(PORT, ()=>{
+    console.log(`The Server is running on Port ${PORT}`);
+
+
+});
+
+
+
