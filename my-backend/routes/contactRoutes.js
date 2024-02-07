c0fc6c8fcc7d006ce25c89f029c1ede019c7f3a9
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2024-02-07 22:36:23 +0500

    Updated

--- my-backend/routes/contactRoutes.js
+++ my-backend/routes/contactRoutes.js
@@ -3,9 +3,41 @@ const router = express.Router();
 
 
 router.route("/").get((req,res)=>{
+res.json({
+        message: 'Hello there this is get '
+    });
+});
 
+router.route("/").post((req,res)=>{
+    res.json({
+            message: 'Hello there create new contact'
+        });
+});
+
+
+router.route("/:id").put((req,res)=>{
+    res.json({
+        message: `Hello there  ${req.params.id}` 
+    });
+});
+
+
+router.route("/:id").delete((req,res)=>{
+    res.json({
+        message: `The Contact is deleted of ${req.params.id}`
+    });
+});
+
+
+router.route("/").get((req,res)=>{
     res.json({
         message: 'Hello there'
+    });
+});
+
+router.route("/:id").get((req,res)=>{
+    res.json({
+        message: `Getting Contact of id ${req.params.id}`
     });
 });
 
