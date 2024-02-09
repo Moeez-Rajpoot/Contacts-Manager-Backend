311a891a8655732a96ef4d32ceaaccee9b1d9cf2
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2024-02-10 00:47:18 +0500

    Updated

--- my-backend/controllers/contactController.js
+++ my-backend/controllers/contactController.js
@@ -1,105 +1,110 @@
-const asynchandler = require('express-async-handler');
-const Contacts = require('../model/contactModel');
-
+const asynchandler = require("express-async-handler");
+const Contacts = require("../model/contactModel");
 
 const getcontact = asynchandler(async (req, res) => {
-
-    try {
-        const Allcontacts = await Contacts.find();
-        
-
-        res.json({
-            message:"All Contacts Displayed",
-            Contacts: Allcontacts
-
-        });
-    } catch (error) {
-
-        res.status(404).json({
-            message:"Error No contacts Found"
-        })
-        
-    }
-
+  try {
     const Allcontacts = await Contacts.find();
-    
-
-
-
-
-
     res.json({
-        message: 'Hello there this is get'
+      message: "All Contacts Displayed",
+      Contacts: Allcontacts,
     });
+  } catch (error) {
+    res.status(404).json({
+      message: "Error No contacts Found",
+    });
+  }
 });
 
 const postcontact = asynchandler(async (req, res) => {
-    console.log("This is Body msg ", req.body);
-    const { name, email, phone } = req.body;
-    if (!name || !email || !phone) {
-        res.status(400);
-        throw new Error("Error in Post Request");
+  console.log("This is Body msg ", req.body);
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("Error in Post Request");
+  }
 
-    }
+  try {
+    const newContact = await Contacts.create({
+      name,
+      email,
+      phone,
+    });
 
-    try {
-        const newContact = await Contacts.create({
-            name,
-            email,
-            phone
-        });
-
-        res.status(201).json({
-            message: 'Contact created successfully',
-            contact: newContact
-        });
-    } catch (error) {
-        console.error('Error creating contact:', error);
-        res.status(404).json({
-            message: 'Internal Server Error'
-        });
-    }
-
+    res.status(201).json({
+      message: "Contact created successfully",
+      contact: newContact,
+    });
+  } catch (error) {
+    console.error("Error creating contact:", error);
+    res.status(404).json({
+      message: "Internal Server Error",
+    });
+  }
 });
-
 
 const putcontact = asynchandler(async (req, res) => {
-    try {
-        const id = req.params;
-        const { name, email, phone } = req.body;
+  try {
+    const id = req.params;
+    const { name, email, phone } = req.body;
 
-        const updatecontact = await Contacts.findByIdAndUpdate({
-            id,
-            name,
-            email,
-            phone
-
-        }, { new: true });
-        res.json({
-            message: "Contact Sucessfully Updated"
-        })
-
-    } catch (error) {
-        res.json({
-            message: "Error While Udating Contact"
-        })
-
-    }
-
+    const updatecontact = await Contacts.findByIdAndUpdate(
+      {
+        id,
+        name,
+        email,
+        phone,
+      },
+      { new: true }
+    );
+    res.json({
+      message: "Contact Sucessfully Updated",
+    });
+  } catch (error) {
+    res.json({
+      message: "Error While Udating Contact",
+    });
+  }
 });
 
-
 const deletecontact = asynchandler(async (req, res) => {
+  try {
+    const ID = req.params.id;
+    const DeleteContact = await Contacts.findByIdAndDelete(ID);
+
     res.json({
-        message: `The Contact is deleted of ${req.params.id}`
+      message: "Contact Deleted Succesfully",
+      Contact: DeleteContact,
     });
+  } catch (error) {
+    res.status(404).json({
+      message: "No Contact Found !",
+    });
+  }
+
+  res.json({
+    message: `The Contact is deleted of ${req.params.id}`,
+  });
 });
 
 const getcontactid = asynchandler(async (req, res) => {
+  const ID = req.params.id;
+  try {
+    const ContactID = await Contacts.findById(ID);
     res.json({
-        message: `Getting Contact of id ${req.params.id}`
+      message: "Contact Found !",
+      Contact: ContactID,
     });
+  } catch (error) {
+    res.json({
+      message: `No Contact Found of ${req.params}`,
+    });
+  }
 });
 
-
-module.exports = { getcontact, postcontact, putcontact, deletecontact, getcontactid };
+module.exports = {
+  getcontact,
+  postcontact,
+  putcontact,
+  deletecontact,
+  getcontactid,
+};
